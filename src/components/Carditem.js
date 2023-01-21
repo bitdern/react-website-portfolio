2afe07eac5d403a1171e9be907dab4564b2e7a96
bitdern <acnewell09@gmail.com>
2023-01-21 16:56:41 -0600

    anotha one

--- src/components/Carditem.js
+++ src/components/Carditem.js
@@ -7,7 +7,7 @@ function Carditem(props) {
       <li className="cards__item">
         <Link className="cards__item__link" to={props.path}>
           <figure className="cards__item__pic-wrap" data-category={props.label}>
-            <img src={props.src} alt="Dern" className="cards__item__img" />
+            <img className="cards__item__img" alt="Dern" src={props.src} />
           </figure>
           <div className="cards__item__info">
             <h5 className="cards__item__text"> {props.text} </h5>
