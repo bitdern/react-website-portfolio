11bda131cd5d92e1b17177515f31618465c81007
bitdern <acnewell09@gmail.com>
2023-02-24 16:31:53 -0600

    testing

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -2,5 +2,5 @@ import React from "react";
 import "../../App.css";
 
 export default function Projects() {
-  return <h1 className="Projects"> Projects </h1>;
+  return <h1 className="Projects"> Projects this is where they would be</h1>;
 }
